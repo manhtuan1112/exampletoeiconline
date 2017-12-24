1195abf71dadd7d3770e3931abbc5fcef17f2a81
Tuan Duong <duongtuan1211@gmail.com>
2017-12-24 22:41:31 +0700

    init project

---
+++ toeic-web-logic/src/main/java/com/mitrais/web/logic/common/WebConstant.java
@@ -0,0 +1,7 @@
+package com.mitrais.web.logic.common;
+
+/**
+ * Created by duongtuan1211 on 12/24/2017.
+ */
+public class WebConstant {
+}
---
+++ toeic-web/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<head></head>
+<body>
+<h1>Test project</h1>
+</body>
+</html>
